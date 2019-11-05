911e2523ae9bc5dd9b366a70dab446272ed42925
yulintainxia <13330810014@qq.com>
2019-11-05 23:42:05 +0800

    diyilai

--- src/app/shared/services/hero-http.service.ts
+++ src/app/shared/services/hero-http.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { HandleError, HttpErrorService } from './http-error.service';
+import { HeroHttp } from '../interfaces/hero-http.interface';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+@Injectable()
 export class HeroHttpService {
+  heroesUrl = 'api/heroes';
+  private handleError: HandleError;
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorService
+  ) {
+    this.handleError = httpErrorHandler.creatHandleError('HeroesService');
+  }
 
-  constructor() { }
+  getHeroes(): Observable<HeroHttp[]> {
+    return this.http.get<HeroHttp[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+  searchHeroes(term: string): Observable<HeroHttp[]> {
+    term = term.trim();
+    const options = term ? {
+      params: new HttpParams().set('name', term)
+    } : {};
+    return this.http.get<HeroHttp[]>(this.heroesUrl, options)
+    .pipe(
+      catchError(this.handleError('searchHeroes', []))
+    );
+  }
 }
