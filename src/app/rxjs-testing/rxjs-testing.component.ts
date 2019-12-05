38776cd23b19cec3c164835760ef9971e913228a
yulintainxia <13330810014@qq.com>
2019-12-05 22:52:36 +0800

    rxjs

--- src/app/rxjs-testing/rxjs-testing.component.ts
+++ src/app/rxjs-testing/rxjs-testing.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { take, tap } from 'rxjs/operators';
+import { take, tap, map, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs-testing',
@@ -9,6 +9,16 @@ import { take, tap } from 'rxjs/operators';
 })
 export class RxjsTestingComponent implements OnInit {
   /* 
+  rxjs:
+ 常见的创建类操作符
+ 1.from: 数组,Promise,以及Iterable转化为Observable;
+ 2.fromEvent:把事件转化为Observable;
+ 3.of:接受一系列数据,并发射出去(可以是单个的值,对象也可以);
+ of({id:1,value:200})
+ 常见的转换操作符:
+ map:原始元素进行处理, map(x=>10*x);
+ mapTo:(常量)
+ pluck:(作用例如直接获取event.taget.value);
   interval是interval 类型创建符（无限）
   time类似timeout的创建符（一个参数）,
   如果有2个参数
@@ -24,6 +34,7 @@ export class RxjsTestingComponent implements OnInit {
   first取第一次操作符
   last 取最后一次
   skip跳过几个,
+  count() 返回流里面的个数
   scan((x,y)=>{return x+y}); 类似递归,第二次x的值是x+y之后的参数的值
   reduce((x,y)=>{return x*y})类似scan，不过只计算最终值,只发射一次
   debunce(()=> interval(300)) 过滤,内部可以接操作符,过滤操作
@@ -37,11 +48,37 @@ export class RxjsTestingComponent implements OnInit {
   combinLatest: 最开始改变,2个流都有值,任何值的改变都会触发流
   zip: 必须成对,第一个流第一项对应第二项的第一项,依次对齐,最慢流决定速度
   withLatestForm: 返回一个数组,2个流都有值,然后第一个流为主,第一个流改变才输出
+   高阶操作符:拍扁(数据流的数据流);
+   flatMap/mergeMap:保留外层所有流的订阅
+    obervable: Observable<any> = interval(100).pipe(
+    take(3),
+    map(val => interval(100)),
+  );
+   this.obervable.subscribe(
+      (val) => {
+         //需要在流里面subscrivbe
+        val.subscribe(vals => console.log(vals));
+      },
+      error => console.log(error),
+      () => console.log('comlplete')
+    );
+    // 拍扁
+     obervable: Observable<any> = interval(100).pipe(
+    take(3),
+    flatMap(val => interval(100)),
+  );
+   this.obervable.subscribe(
+      (val) => {
+         console.log(val)
+      },
+      error => console.log(error),
+      () => console.log('comlplete')
+    );
+    switchMap:有外层流进来,原来的的外层流抛弃掉
   */
   obervable: Observable<any> = interval(100).pipe(
     take(3),
-    tap(val => console.log('当前值' + val)
-    )
+    flatMap(val => interval(100)),
   );
   constructor() { }
 
@@ -49,13 +86,15 @@ export class RxjsTestingComponent implements OnInit {
     this.testing();
   }
   testing() {
-    this.obervable.subscribe(
-      (val) => {
-        console.log(val);
-      },
-      error => console.log(error),
-      () => console.log('comlplete')
-    );
+    // this.obervable.subscribe(
+    //   (val) => {
+    //     // console.log(val);
+    //     // console.log(val);
+    //     // val.subscribe(vals => console.log(vals));
+    //   },
+    //   error => console.log(error),
+    //   () => console.log('comlplete')
+    // );
   }
 
 }
